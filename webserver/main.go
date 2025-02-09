bae6ee84e5aaa2fe452bac1c891747730bf44158
KesseNones <creepertron01@gmail.com>
2025-02-08 16:13:10 -0800

    Added /receipts/process endpoint and basic processing
    It's not known if the Unmarshal function actually worked in
    parsing the input JSON, but it doesn't blow up so that's a start
    at least.

--- webserver/main.go
+++ webserver/main.go
@@ -10,55 +10,56 @@ import (
 	"io/ioutil"
 )
 
+type ReceiptItem struct{
+	ShortDesc string `json:"shortDescription"`
+	Price string `json:"price"`
+}
+
+//Input struct used to handle either receipt input or just id.
+type Receipt struct{
+	Retailer string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	Total string `json:"total"`
+	Items []ReceiptItem `json:"items"`
+}
+
 type DataBase struct {
-	names []string
+	Data map[string]int
 }
 
-type Input struct{
-	Name string `json:"name"`
-}
-
-//Adds posted name to db.
-func (db *DataBase) NameHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Println(uuid.New().String())
-	if r.Method == http.MethodGet{
-		fmt.Println("GET REQUEST")
-		if len(db.names) == 0{
-			fmt.Fprintf(w, "NO NAMES\n")
-		}
-		for i := range db.names{
-			fmt.Fprintf(w, "%s\n", db.names[i])
-		}
-	}else if r.Method == http.MethodPost{
-		fmt.Println("POST REQUEST")
-	
-		body, err0 := ioutil.ReadAll(r.Body)
-		if err0 != nil{
-			fmt.Println("STUFF BROKE HARD")
-			return
-		}
-
-		var input Input
-		
-		err := json.Unmarshal(body, &input)
+//Takes in the input receipt, calculates point value, 
+// and writes points to database.
+func (db *DataBase) InsertToDatabase(w http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodPost{
+		requestBody, err := ioutil.ReadAll(r.Body)
 		if err != nil{
-			fmt.Println("STUFF BROKE", err)
+			//ADD REAL ERROR HERE!
+			fmt.Println("FAILURE READING!!!")
 			return
 		}
-			
-		db.names = append(db.names, input.Name)
+
+		var receipt Receipt
+
+		marshErr := json.Unmarshal(requestBody, &receipt)
+		if marshErr != nil{
+			fmt.Println("FAILED TO PARSE BECAUSE:", marshErr)
+			return
+		}
+
+		fmt.Println("SUCCESS:", uuid.New().String())
+
+		//Calculate points here!
+
 	}else{
-		fmt.Fprintf(w, "ERROR! Request must be a GET or POST!")
+		//ADD REAL ERROR HERE LATER!!!
+		fmt.Println("BAD REQUEST!!!!")
 	}
 }
 
 func main(){
-	data := &DataBase{
-		names: []string{},
-	}
+	data := DataBase{map[string]int{}}
 
-	http.HandleFunc("/names", data.NameHandler)
-	//http.HandleFunc("/names", data.DisplayNames) 
+	http.HandleFunc("/receipts/process", data.InsertToDatabase)
 
 	fmt.Println("Listening on Port 8000!")
 	
