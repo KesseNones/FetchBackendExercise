17ec5104c52fd562a0b9f1838136e2b539e57049
KesseNones <creepertron01@gmail.com>
2025-02-09 16:26:18 -0800

    Added purchase time to receipt struct
    Somehow forgot to add purchase time so now it's there.

--- webserver/main.go
+++ webserver/main.go
@@ -23,6 +23,7 @@ type ReceiptItem struct{
 type Receipt struct{
 	Retailer string `json:"retailer"`
 	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
 	Total string `json:"total"`
 	Items []ReceiptItem `json:"items"`
 }
